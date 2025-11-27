8a52a718db120bf8ee9cf879e6045793ab6f874e
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-11-27 23:08:49 +1100

    Mapper name

--- showcase/index.js
+++ showcase/index.js
@@ -18,10 +18,16 @@ let beatmapId, beatmapChecksum
 const identifierNameEl = document.getElementById("identifier-name")
 const modNameContainerEl = document.getElementById("mod-name-container")
 const modNameEl = document.getElementById("mod-name")
+// Title
 const titleScrollEl = document.getElementById("title-scroll")
 const titleName1El = document.getElementById("title-name-1")
 const titleName2El = document.getElementById("title-name-2")
 const titleName3El = document.getElementById("title-name-3")
+// Mapper
+const mapperScrollEl = document.getElementById("mapper-scroll")
+const mapperName1El = document.getElementById("mapper-name-1")
+const mapperName2El = document.getElementById("mapper-name-2")
+const mapperName3El = document.getElementById("mapper-name-3")
 
 const socket = createTosuWsSocket()
 socket.onmessage = async event => {
@@ -39,19 +45,40 @@ socket.onmessage = async event => {
         titleName1El.textContent = authorTitle
         titleName2El.textContent = authorTitle
         titleName3El.textContent = authorTitle
-        const currentWidth = Math.round(
+        const titleWidth = Math.round(
             Math.max(titleName1El.getBoundingClientRect().width,
                     titleName2El.getBoundingClientRect().width)
         )
-        if (currentWidth > 396) {
+        if (titleWidth > 396) {
             titleName1El.style.display = "none"
             titleScrollEl.style.display = "block"
             titleName3El.style.transform = `translateX(calc(100% + 14px))`
-            const timing = 45 * currentWidth
+            const timing = 45 * titleWidth
             document.documentElement.style.setProperty("--title-name-animation-time", `${timing}ms`)
         } else {
             titleName1El.style.display = "block"
             titleScrollEl.style.display = "none"
+        }
+        // Mapper
+        const mapper = data.beatmap.mapper
+        mapperName1El.textContent = mapper
+        mapperName2El.textContent = mapper
+        mapperName3El.textContent = mapper
+        console.log(mapperName1El.getBoundingClientRect().width, mapperName2El.getBoundingClientRect().width)
+        const mapperName = Math.round(
+            Math.max(mapperName1El.getBoundingClientRect().width,
+                    mapperName2El.getBoundingClientRect().width)
+        )
+        console.log(mapperName)
+        if (mapperName > 396) {
+            mapperName1El.style.display = "none"
+            mapperScrollEl.style.display = "block"
+            mapperName3El.style.transform = `translateX(calc(100% + 14px))`
+            const timing = 45 * titleWidth
+            document.documentElement.style.setProperty("--mapper-name-animation-time", `${timing}ms`)
+        } else {
+            mapperName1El.style.display = "block"
+            mapperScrollEl.style.display = "none"
         }
 
         const beatmap = findShowcaseBeatmap(beatmapId)
