7768409edd6d1d0cb701dab2dc09c970076d2636
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-12-03 14:46:26 +1100

    Make adjustments to get beatmaps json for current and future use

--- get-beatmaps-json/index.js
+++ get-beatmaps-json/index.js
@@ -1,3 +1,5 @@
+import { delay } from "../_shared/core/utils.js"
+
 // Load osu! api
 let osuApi
 async function getApi() {
@@ -9,10 +11,12 @@ async function getApi() {
 let allBeatmapsJson = []
 let fullJson = []
 
+const downloadingDataEl = document.getElementById("downloading-data")
+
 async function getBeatmaps() {
     const response = await fetch("../_data/beatmaps-base.json")
     const responseJson = await response.json()
-    allBeatmaps = responseJson.beatmaps
+    const allBeatmaps = responseJson.beatmaps
 
     for (let i = 0; i < allBeatmaps.length; i++) {
         // Set mod number
@@ -25,9 +29,57 @@ async function getBeatmaps() {
         const response = await fetch(`https://api.codetabs.com/v1/proxy?quest=` + encodeURIComponent(`https://osu.ppy.sh/api/get_beatmaps?k=${osuApi}&b=${allBeatmaps[i].beatmap_id}&mods=${modNumber}`))
         await delay(1000)
         let responseJson = await response.json()
-        responseJson[0].mod = allBeatmaps[i].mod
-        responseJson[0].order = allBeatmaps[i].order
-        if (allBeatmaps[i].mod === "FM" || allBeatmaps[i].mod === "TB") responseJson[0].EZMultiplier = allBeatmaps[i].EZMultiplier
+
+        downloadingDataEl.textContent = `Downloading Data: ${i + 1} / ${allBeatmaps.length}`
+
+        responseJson[0] = {
+            // "approved": responseJson[0].approved,
+            // "approved_date": responseJson[0].approved_date,
+            // "artist": responseJson[0].artist,
+            // "artist_unicode": responseJson[0].artist_unicode,
+            // "audio_unavailable": responseJson[0].audio_unavailable,
+            "beatmap_id": responseJson[0].beatmap_id,
+            "beatmapset_id": responseJson[0].beatmapset_id,
+            // "bpm": responseJson[0].bpm,
+            // "count_normal": responseJson[0].count_normal,
+            // "count_slider": responseJson[0].count_slider,
+            // "count_spinner": responseJson[0].count_spinner,
+            // "creator": responseJson[0].creator,
+            // "creator_id": responseJson[0].creator_id,
+            // "diff_aim": responseJson[0].diff_aim,
+            // "diff_approach": responseJson[0].diff_approach,
+            // "diff_drain": responseJson[0].diff_drain,
+            // "diff_overall": responseJson[0].diff_overall,
+            // "diff_size": responseJson[0].diff_size,
+            // "diff_speed": responseJson[0].diff_speed,
+            // "difficultyrating": responseJson[0].difficultyrating,
+            // "download_unavailable": responseJson[0].download_unavailable,
+            // "favourite_count": responseJson[0].favourite_count,
+            // "file_md5": responseJson[0].file_md5,
+            // "genre_id": responseJson[0].genre_id,
+            // "hit_length": responseJson[0].hit_length,
+            // "language_id": responseJson[0].language_id,
+            // "last_update": responseJson[0].last_update,
+            // "max_combo": responseJson[0].max_combo,
+            // "mode": responseJson[0].mode,
+            // "packs": responseJson[0].packs,
+            // "passcount": responseJson[0].passcount,
+            // "playcount": responseJson[0].playcount,
+            // "rating": responseJson[0].rating,
+            // "source": responseJson[0].source,
+            // "storyboard": responseJson[0].storyboard,
+            // "submit_date": responseJson[0].submit_date,
+            // "tags": responseJson[0].tags,
+            // "title": responseJson[0].title,
+            // "title_unicode": responseJson[0].title_unicode,
+            // "total_length": responseJson[0].total_length,
+            // "version": responseJson[0].version,
+            // "video": responseJson[0].video
+        }
+
+        console.log(allBeatmaps[i])
+        responseJson[0].identifier = allBeatmaps[i].identifier
+        responseJson[0].mod = allBeatmaps[i].mod        
         
         allBeatmapsJson.push(responseJson[0])
     }
@@ -46,6 +98,13 @@ async function getBeatmaps() {
 }
 
 async function initialise() {
+    buttonEl.disabled = true
     await getApi()
     await getBeatmaps()
+    buttonEl.disabled = false
 }
+
+const buttonEl = document.getElementById("button")
+window.addEventListener("load", () => {
+    buttonEl.addEventListener("click", initialise)
+})
