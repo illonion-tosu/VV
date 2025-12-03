b4aae259302221c8ef6257235766021c6a2e7554
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-12-03 15:42:55 +1100

    Add team information (untested)

--- main/index.js
+++ main/index.js
@@ -1,4 +1,5 @@
-import { loadBeatmaps, findBeatmap } from "../_shared/core/data.js"
+import { loadBeatmaps, findBeatmap, loadTeams, findTeam } from "../_shared/core/data.js"
+import { createTosuWsSocket } from "../_shared/core/websocket.js"
 
 // Round names
 const roundNameFrontEl = document.getElementById("round-name-front")
@@ -6,8 +7,50 @@ const roundNameBackEl = document.getElementById("round-name-back")
 
 // Load beatmaps and players
 let allBeatmaps = []
-Promise.all([loadBeatmaps()]).then(([beatmaps]) => {
+let allTeams = []
+Promise.all([loadBeatmaps(), loadTeams()]).then(([beatmaps, teams]) => {
+    allTeams = teams
     allBeatmaps = beatmaps
     roundNameFrontEl.innerText = allBeatmaps.roundName
     roundNameBackEl.setAttribute("src", `static/rounds/${allBeatmaps.roundName}.png`)
 })
+
+// Team Information
+// Team Avatars
+const redTeamAvatarEl = document.getElementById("red-team-avatar")
+const blueTeamAvatarEl = document.getElementById("blue-team-avatar")
+// Team Names
+const redTeamNameEl = document.getElementById("red-team-name")
+const blueTeamNameEl = document.getElementById("blue-team-name")
+// Team Seed Number
+const redTeamSeedNumberEl = document.getElementById("red-team-seed-number")
+const blueTeamSeedNumberEl = document.getElementById("blue-team-seed-number")
+// Variables
+let redTeamName, blueTeamName
+
+// Socket
+const socket = createTosuWsSocket()
+socket.onmessage = async event => {
+    const data = JSON.parse(event.data)
+    console.log(data)
+
+    if (redTeamName !== data.tourney.team.left) {
+        redTeamName = setTeamDisplays(data.tourney.team.left, redTeamNameEl, redTeamAvatarEl, redTeamSeedNumberEl)
+    }
+    if (blueTeamName !== data.tourney.team.right) {
+        blueTeamName = setTeamDisplays(data.tourney.team.right, blueTeamNameEl, blueTeamAvatarEl, blueTeamSeedNumberEl)
+    }
+}
+
+// Set Team Displays
+function setTeamDisplays(teamName, teamNameElement, avatarElement, seedNumberElement) {
+    teamNameElement.textContent = teamName
+
+    const team = findTeam(teamName)
+    if (team) {
+        // TOOD: Set actual team name
+        avatarElement.style.backgroundImage = `url("https://a.ppy.sh/2")`
+        seedNumberElement.textContent = team.seed
+    }
+    return teamName
+}
