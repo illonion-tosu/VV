9e6a3e1db53b41af56659f842bbe56f48d3e33ee
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-12-05 14:02:42 +1100

    Complete stats

--- main/index.js
+++ main/index.js
@@ -40,6 +40,23 @@ let chatLen = 0
 // Score visibility
 let scoreVisible
 
+// Now Playing Container
+const nowPlayingContainerEl = document.getElementById("now-playing-container")
+const nowPlayingBorderEl = document.getElementById("now-playing-border")
+const nowPlayingArtistEl = document.getElementById("now-playing-artist")
+const nowPlayingSongTitleEl = document.getElementById("now-playing-song-title")
+const nowPlayingDifficultyEl = document.getElementById("now-playing-difficulty")
+const nowPlayingMapperEl = document.getElementById("now-playing-mapper")
+// Stats
+const nowPlayingSrNumberEl = document.getElementById("now-playing-sr-number")
+const nowPlayingLenNumberEl = document.getElementById("now-playing-len-number")
+const nowPlayingBpmNumberEl = document.getElementById("now-playing-bpm-number")
+const nowPlayingArNumberEl = document.getElementById("now-playing-ar-number")
+const nowPlayingCsNumberEl = document.getElementById("now-playing-cs-number")
+const nowPlayingHpNumberEl = document.getElementById("now-playing-hp-number")
+const nowPlayingOdNumberEl = document.getElementById("now-playing-od-number")
+let beatmapId, beatmapChecksum, updateStats = false
+
 // Socket
 const socket = createTosuWsSocket()
 socket.onmessage = async event => {
@@ -99,6 +116,60 @@ socket.onmessage = async event => {
             chatDisplayLiveTitleTextEl.style.opacity = 1
         }
     }
+
+    // Now Playing
+    const beatmapData = data.beatmap
+    if (beatmapId !== data.beatmap.id || beatmapChecksum !== data.beatmap.checksum) {
+        beatmapId = beatmapData.id
+        beatmapChecksum = beatmapData.checksum
+        updateStats = undefined
+
+        // Metadata
+        nowPlayingContainerEl.style.backgroundImage = `url("https://assets.ppy.sh/beatmaps/${data.beatmap.set}/covers/cover.jpg")`
+        nowPlayingArtistEl.textContent = beatmapData.artist
+        nowPlayingSongTitleEl.textContent = beatmapData.title
+        nowPlayingDifficultyEl.textContent = beatmapData.version
+        nowPlayingMapperEl.textContent = beatmapData.mapper
+
+        updateStats = findBeatmap(beatmapId)
+        if (updateStats) {
+            const currentMap = updateStats
+            console.log("undefined")
+            const stats = getStats(Number(currentMap.difficultyrating), Number(currentMap.diff_approach),
+                Number(currentMap.diff_size), Number(currentMap.diff_overall), Number(currentMap.diff_drain),
+                Number(currentMap.bpm), Number(currentMap.total_length), currentMap.mod
+            )
+
+            nowPlayingSrNumberEl.textContent = stats.sr
+            nowPlayingLenNumberEl.textContent = setLengthDisplay(stats.len)
+            nowPlayingBpmNumberEl.textContent = stats.bpm
+            nowPlayingArNumberEl.textContent = stats.ar
+            nowPlayingCsNumberEl.textContent = stats.cs
+            nowPlayingHpNumberEl.textContent = stats.hp
+            nowPlayingOdNumberEl.textContent = stats.od
+            updateStats = false
+        } else {
+            await delay(250)
+            updateStats = true
+        }
+    }
+
+    // Update stats
+    if (updateStats) {
+        const statsObj = beatmapData.stats
+        const stats = getStats(statsObj.stars.total, statsObj.ar.converted, statsObj.cs.converted,
+            statsObj.od.converted, statsObj.hp.converted, statsObj.bpm.common,
+            Math.round((beatmapData.time.lastObject - beatmapData.time.firstObject) / 1000), "NM"
+        )
+
+        nowPlayingSrNumberEl.textContent = stats.sr
+        nowPlayingLenNumberEl.textContent = setLengthDisplay(stats.len)
+        nowPlayingBpmNumberEl.textContent = stats.bpm
+        nowPlayingArNumberEl.textContent = stats.ar
+        nowPlayingCsNumberEl.textContent = stats.cs
+        nowPlayingHpNumberEl.textContent = stats.hp
+        nowPlayingOdNumberEl.textContent = stats.od
+    }
 }
 
 // Set Team Displays
@@ -112,4 +183,32 @@ function setTeamDisplays(teamName, teamNameElement, avatarElement, seedNumberEle
         seedNumberElement.textContent = team.seed
     }
     return teamName
+}
+
+// Get Stats
+function getStats(sr, ar, cs, od, hp, bpm, len, mod) {
+    if (mod.includes("HR")) {
+        cs = Math.min(Math.round(cs * 1.3 * 10) / 10, 10)
+        ar = Math.min(Math.round(ar * 1.4 * 10) / 10, 10)
+        hp = Math.min(Math.round(hp * 1.4 * 10) / 10, 10)
+        od = Math.min(Math.round(od * 1.4 * 10) / 10, 10)
+    }
+    if (mod.includes("DT") || mod.includes("NC")) {
+        if (ar > 5) ar = Math.round((((1200 - (( 1200 - (ar - 5) * 150) * 2 / 3)) / 150) + 5) * 10) / 10
+        else ar = Math.round((1800 - ((1800 - ar * 120) * 2 / 3)) / 120 * 10) / 10
+        if (hp > 5) hp = Math.round((((1200 - (( 1200 - (hp - 5) * 150) * 2 / 3)) / 150) + 5) * 10) / 10
+        else hp = Math.round((1800 - ((1800 - hp * 120) * 2 / 3)) / 120 * 10) / 10
+        od = Math.round((79.5 - (( 79.5 - 6 * od) * 2 / 3)) / 6 * 10) / 10
+        bpm = Math.round(bpm * 1.5)
+        len = Math.round(len / 1.5)
+    }
+    return {sr: sr, ar: ar, cs: cs, hp: hp, od: od, bpm: bpm, len: len, mod: mod}
+}
+
+// Set Length Display
+function setLengthDisplay(seconds) {
+    const minuteCount = Math.floor(seconds / 60)
+    const secondCount = seconds % 60
+
+    return `${minuteCount.toString()}:${secondCount.toString().padStart(2, "0")}`
 }
