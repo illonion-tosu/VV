dc95b38d97bec3ea9a4073693396514dba5b0a3a
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-12-20 18:51:31 +1100

    Complete Chat

--- mappool/index.js
+++ mappool/index.js
@@ -212,6 +212,10 @@ const toggleAutopickButtonEl = document.getElementById("toggle-autopick-button")
 const toggleAutopickOnOffEl = document.getElementById("toggle-autopick-on-off")
 let isAutopickOn = false, currentPicker = "red"
 
+// Chat Display
+const chatDisplayContainerEl = document.getElementById("chat-display-container")
+let chatLen
+
 // Socket
 const socket = createTosuWsSocket()
 socket.onmessage = async event => {
@@ -278,6 +282,32 @@ socket.onmessage = async event => {
             if (currentPicker === "red") setAutopicker("blue")
             else if (currentPicker === "blue") setAutopicker("red")
         }
+    }
+
+    // This is also mostly taken from Victim Crasher: https://github.com/VictimCrasher/static/tree/master/WaveTournament
+    if (chatLen !== data.tourney.chat.length) {
+        (chatLen === 0 || chatLen > data.tourney.chat.length) ? (chatDisplayContainerEl.innerHTML = "", chatLen = 0) : null
+        const fragment = document.createDocumentFragment()
+
+        for (let i = chatLen; i < data.tourney.chat.length; i++) {
+            // Message container
+            const messageContainer = document.createElement("div")
+
+            // Name
+            const messageName = document.createElement("span")
+            messageName.classList.add(data.tourney.chat[i].team)
+            messageName.textContent = `${data.tourney.chat[i].name}: `
+
+            // Message
+            const messageContent = document.createElement("span")
+            messageContent.textContent = data.tourney.chat[i].message
+
+            messageContainer.append(messageName, messageContent)
+            fragment.append(messageContainer)
+        }
+
+        chatDisplayContainerEl.append(fragment)
+        chatLen = data.tourney.chat.length
     }
 }
 
